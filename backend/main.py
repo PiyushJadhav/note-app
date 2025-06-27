d9abb65356ce29ddbc0e4b893c1f1f78b1caac18
Piyush Jadhav <pjadhav@ucsb.edu>
2025-06-26 22:03:02 -0700

    added gitignore and 1 more route

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    email: str
+    age: int
 
 app = FastAPI()
 
@@ -6,4 +13,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "hi!"}
+
+@app.post("/info")
+async def info(inf : Item):
+    item_dic = inf.dict()
+    if 'pj' in item_dic or 'pj' in item_dic.values():
+        return {"message": "success!"}
+    else:
+        return {"message": "failure!"}
 
